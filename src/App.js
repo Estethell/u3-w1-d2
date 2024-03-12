c1d34757f3fed070e948a78d9e85cbabc22d7bed
Ilaria Rosati <156794733+Estethell@users.noreply.github.com>
2024-03-12 14:09:13 +0100

    u3-w1-d2

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import "./components/MyNav";
+import MyNav from "./components/MyNav";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function App() {
+  return (
+    <div>
+      <MyNav />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/MyFooter.jsx
@@ -0,0 +1,5 @@
+const MyFooter = () => {
+  <footer></footer>;
+};
+
+export default MyFooter;
